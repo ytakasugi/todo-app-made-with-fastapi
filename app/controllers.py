27d0d44f6123a4895c8387cb44dd01678a9e4a75
ytakasugi <sh7.tibi0129@gmail.com>
2022-05-01 19:46:45 +0900

    change file

--- app/controllers.py
+++ app/controllers.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 import re
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Form
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from starlette.templating import Jinja2Templates
@@ -213,3 +213,77 @@ async def add(request: Request, credentials: HTTPBasicCredentials = Depends(secu
 
     return RedirectResponse('/admin')
 
+def delete(request: Request, t_id, credentials: HTTPBasicCredentials = Depends(security)):
+    # 認証
+    username = auth(credentials)
+
+    # ログインユーザー取得
+    user = db.session.query(User).filter(User.username == username).first()
+
+    # 該当タスクを取得
+    task = db.session.query(Task).filter(Task.id == t_id).first()
+    # もし、ユーザーIDが異なれば削除せずリダイレクト
+    if task.user_id != user.id:
+        return RedirectResponse('/admin')
+
+    # 削除してコミット
+    db.session.delete(task)
+    db.session.commit()
+    db.session.close()
+
+    return RedirectResponse('/admin')
+
+def get(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    # 認証
+    username = auth(credentials)
+
+    # ユーザ情報を取得
+    user = db.session.query(User).filter(User.username == username).first()
+
+    # タスクを取得
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+
+    db.session.close()
+
+    # JSONフォーマット
+    task = [{
+        'id': t.id,
+        'content': t.content,
+        'deadline': t.deadline.strftime('%Y-%m-%d %H:%M:%S'),
+        'published': t.date.strftime('%Y-%m-%d %H:%M:%S'),
+        'done': t.done,
+    } for t in task]
+
+    return task
+
+
+async def insert(request: Request,
+                 content: str = Form(...), deadline: str = Form(...),
+                 credentials: HTTPBasicCredentials = Depends(security)):
+    """
+    タスクを追加してJSONで新規タスクを返す。「deadline」は%Y-%m-%d_%H:%M:%S (e.g. 2019-11-03_12:30:00)の形式
+    """
+    # 認証
+    username = auth(credentials)
+
+    # ユーザ情報を取得
+    user = db.session.query(User).filter(User.username == username).first()
+
+    # タスクを追加
+    task = Task(user.id, content, datetime.strptime(deadline, '%Y-%m-%d_%H:%M:%S'))
+
+    db.session.add(task)
+    db.session.commit()
+
+    # テーブルから新しく追加したタスクを取得する
+    task = db.session.query(Task).all()[-1]
+    db.session.close()
+
+    # 新規タスクをJSONで返す
+    return {
+        'id': task.id,
+        'content': task.content,
+        'deadline': task.deadline.strftime('%Y-%m-%d %H:%M:%S'),
+        'published': task.date.strftime('%Y-%m-%d %H:%M:%S'),
+        'done': task.done,
+    }
--- app/urls.py
+++ app/urls.py
@@ -6,3 +6,8 @@ app.add_api_route('/register', register, methods=['GET', 'POST'])
 app.add_api_route('/todo/{username}/{year}/{month}/{day}', detail)  
 app.add_api_route('/done', done, methods=['POST'])
 app.add_api_route('/add', add, methods=['POST'])
+app.add_api_route('/delete/{t_id}', delete)
+
+# JSONで返すAPI
+app.add_api_route('/get', get)
+app.add_api_route('/add_task', insert, methods=['POST'])
